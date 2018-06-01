a92df614e35767ccd8b4ce7d9bf47a9b14ea3670
Philip Tenteromano <tenteromano.p@gmail.com>
2018-06-01 17:02:01 -0400

    styling and guard

--- spec/features/creating_article_spec.rb
+++ spec/features/creating_article_spec.rb
@@ -25,5 +25,20 @@ RSpec.feature "creating articles" do
     expect(page).to have_content("Article has been created")
     expect(page.current_path).to eq(articles_path)
   end
+
+  scenario "user fails to create a new article" do
+    visit "/"
+
+    click_link "New Article"
+
+    fill_in "Title", with: ""
+    fill_in "Body", with: ""
+
+    click_button "Create Article"
+
+    expect(page).to have_content("Article has not been created")
+    expect(page).to have_content("Title can't be blank")
+    expect(page).to have_content("Body can't be blank")
+  end
 end
 
