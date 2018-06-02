516bae0984fc8aed0d4ab884bbfc89b897168e03
Philip Tenteromano <tenteromano.p@gmail.com>
2018-06-01 22:20:09 -0400

    show article testing

---
+++ spec/features/show_article_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.feature "showing an article" do
+
+  before do
+    @article1 = Article.create(title: "Article title", body: "this is the body")
+  end
+
+  scenario "user views a specific article" do
+    visit "/"
+
+    click_link @article1.title
+
+    expect(page).to have_content(@article1.title)
+    expect(page).to have_content(@article1.body)
+    expect(current_path).to eq(article_path(@article1))
+
+  end
+
+end
