5538f1bce7af8da4e9627c2228b283cb5779f141
Philip Tenteromano <tenteromano.p@gmail.com>
2018-06-04 14:56:35 -0400

    devise test and view styling

---
+++ spec/features/signup_user_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.feature "Signup user" do
+
+  scenario "With valid credentials" do
+    visit "/"
+
+    click_link "Sign Up"
+
+    fill_in "Email", with: "user@example.com"
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "password"
+
+    click_button "Sign up"
+
+    expect(page).to have_content("You have signed up successfully")
+  end
+
+  scenario "With invalid credentials" do
+    visit "/"
+
+    click_link "Sign Up"
+
+    fill_in "Email", with: ""
+    fill_in "Password", with: ""
+    fill_in "Password confirmation", with: ""
+
+    click_button "Sign up"
+
+    # expect(page).to have_content("Error signing up")
+  end
+
+end
